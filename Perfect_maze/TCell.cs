26ad91af2b8961ce86f75a60d9f3ebbda2f86167
Magdalena Leśniewska <119002735+Meggi10@users.noreply.github.com>
2026-03-17 11:53:17 +0100

    Fix movement bug
    This version of project contains fix movement bug after reset the whole maze

---
+++ Perfect_maze/TCell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_maze
+{
+    public class TCell
+    {
+        public int X, Y;
+        public List<TCell> Connected = new List<TCell>();
+
+    }
+}
