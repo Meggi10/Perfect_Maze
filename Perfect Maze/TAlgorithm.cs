ab97e1d5a0e2a858212348d1b2dc12611eb10be5
Meggi10 <magdakenobi10@gmail.com>
2026-07-12 14:38:53 +0200

    Fixes BFS and A* algorithms. The implementation was wrong in both and doesn't work previously as should.

--- Perfect Maze/TAlgorithm.cs
+++ Perfect Maze/TAlgorithm.cs
@@ -11,53 +11,43 @@ namespace Perfect_maze
 {
     public class TAlgorithm
     {
-        public static TCell BFS(TCell root, TCell goal)
+        public static List<TCell> BFS(TCell root, TCell goal)
         {
             var queue = new Queue<TCell>();
             var explored = new HashSet<TCell>() { root };
+            var cameFrom = new Dictionary<TCell, TCell>();
             queue.Enqueue(root);
             while (queue.Count > 0)
             {
                 var visited = queue.Dequeue();
                 if (visited == goal)
-                    return visited;
+                    return ReconstructPath(cameFrom, goal);
                 foreach (var neighbour in visited.Connected)
                 {
                     if (!explored.Contains(neighbour))
                     {
                         explored.Add(neighbour);
-                        neighbour.Parent = visited;
+                        cameFrom[neighbour] = visited;
                         queue.Enqueue(neighbour);
                     }
                 }
             }
             return null;
         }
-        public static List<TCell> GetPath(TCell goalCell)
-        {
-            var path = new List<TCell>();
-            var current = goalCell;
-            while (current != null)
-            {
-                path.Add(current);
-                current = current.Parent;
-            }
-            path.Reverse();
-            return path;
-        }
         public static List<TCell> AStar(TCell root, TCell goal)
         {
             var cellsToCheck = new HashSet<TCell> { root };
             var cellsChecked = new HashSet<TCell>();
             var stepFromStart = new Dictionary<TCell, double> { [root] = 0 };
+            var cameFrom = new Dictionary<TCell, TCell>();
             while (cellsToCheck.Count > 0)
             {
                 TCell current = null;
                 double lowestF = double.MaxValue;
                 foreach (var node in cellsToCheck)
                 {
-                    double g = stepFromStart.ContainsKey(node) ? stepFromStart[node] : double.MaxValue;
-                    double heuristic = Math.Abs(root.X - goal.X) + Math.Abs(root.Y - goal.Y);
+                    double g = stepFromStart[node];
+                    double heuristic = Math.Abs(node.X - goal.X) + Math.Abs(node.Y - goal.Y);
                     double f = g + heuristic;
                     if (f < lowestF)
                     {
@@ -65,26 +55,41 @@ namespace Perfect_maze
                         current = node;
                     }
                 }
+                if (current == goal)
+                    return ReconstructPath(cameFrom, goal);
                 cellsToCheck.Remove(current);
                 cellsChecked.Add(current);
-                if (current == goal)
-                    return GetPath(goal);
                 foreach (var neighbour in current.Connected)
                 {
                     if (cellsChecked.Contains(neighbour))
                         continue;
+
                     double stepCost = stepFromStart[current] + 1;
-                    var gg = stepFromStart.ContainsKey(neighbour) ? stepFromStart[neighbour] : double.MaxValue;
-                    if (stepCost < gg || !cellsToCheck.Contains(neighbour))
+                    double previousCost = stepFromStart.ContainsKey(neighbour) ? stepFromStart[neighbour] : double.MaxValue;
+
+                    if (stepCost < previousCost)
                     {
-                        neighbour.Parent = current;
+                        cameFrom[neighbour] = current;
                         stepFromStart[neighbour] = stepCost;
+
                         if (!cellsToCheck.Contains(neighbour))
                             cellsToCheck.Add(neighbour);
                     }
                 }
             }
             return null;
+        }
+        private static List<TCell> ReconstructPath(Dictionary<TCell, TCell> cameFrom, TCell goal)
+        {
+            var path = new List<TCell> { goal };
+            var current = goal;
+            while (cameFrom.ContainsKey(current))
+            {
+                current = cameFrom[current];
+                path.Add(current);
+            }
+            path.Reverse();
+            return path;
         }
     }
 }
