5a266c24b4e2aaa2d91ac4d788c87b8e8b7385ba
Meggi10 <magdakenobi10@gmail.com>
2026-07-12 12:59:26 +0200

    Revert "Simple UI update"
    This reverts commit 4aa10faf61d8b420b584e2fcfd7d79f514a8897a.

--- Perfect Maze/Form2.cs
+++ Perfect Maze/Form2.cs
@@ -26,14 +26,14 @@ namespace Perfect_maze
         {
             Form1 form1 = new Form1();
             form1.Show();
-            Hide();
+            this.Hide();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Game_Rules game_rules = new Game_Rules();
             game_rules.Show();
-            Hide();
+            this.Hide();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -50,13 +50,6 @@ namespace Perfect_maze
                  MessageBoxIcon.Question);
             if (result == DialogResult.Yes)
                 Application.Exit();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Credits credits = new Credits();
-            credits.Show();
-            Hide();
         }
     }
 }
