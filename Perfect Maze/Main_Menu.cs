1e3263701efa6fdccafa270493eea327b5cc4620
Meggi10 <magdakenobi10@gmail.com>
2026-08-01 11:13:29 +0200

    Adding Main Menu form

---
+++ Perfect Maze/Main_Menu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perfect_maze
+{
+    public partial class Main_Menu : Form
+    {
+        public Main_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Game form1 = new Game();
+            form1.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Game_Rules game_rules = new Game_Rules();
+            game_rules.Show();
+            this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Are you sure, you want to exit?",
+                "Exit",
+                 MessageBoxButtons.YesNo,
+                 MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Credits credits = new Credits();
+            credits.Show();
+            this.Hide();
+        }
+    }
+}
