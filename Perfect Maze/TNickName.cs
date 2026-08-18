707467cd3620f29a09e64bc210cbd7e9e3ac7fa1
Meggi10 <magdakenobi10@gmail.com>
2026-08-18 18:25:13 +0200

    Add class TNickName for JSON read and deserialize and method GetRandomNickName to get random name from the file

---
+++ Perfect Maze/TNickName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Perfect_maze
+{
+    public static class TNickName
+    {
+        private static List<string> nickName = new List<string>();
+        private static readonly Random random = new Random();
+
+        public static void Load()
+        {
+            string json = TResourceReader.ReadManifestText("Data_.NickNames.json");
+            var data = JsonSerializer.Deserialize<TNickNameData>(json);
+            nickName = data?.NickNames ?? new List<string>();
+        }
+
+        public static string GetRandomNickName()
+        {
+            if (nickName.Count == 0) return string.Empty;
+            int index = random.Next(nickName.Count);
+            return nickName[index];
+        }
+    }
+    public class TNickNameData
+    {
+        public List<string> NickNames { get; set; }
+    }
+}
