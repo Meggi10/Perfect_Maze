7c3ff4a3d9ceeba11f2d54adddd0cdaeeaef0d7d
Meggi10 <magdakenobi10@gmail.com>
2026-08-18 18:23:49 +0200

    Add TSession class to save player Name in active session - display in the Game

---
+++ Perfect Maze/TSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_maze
+{
+    public static class TSession
+    {
+        public static string PlayerName { get; set; }
+        public static List<string> Names { get; set; } = new List<string>();
+        public static void Clear()
+        {
+            PlayerName = null;
+            Names.Clear();
+        }
+    }
+}
