97ad9805eff932c1364f4a0b43fc7a837b097745
Meggi10 <magdakenobi10@gmail.com>
2026-08-18 18:26:20 +0200

    Add TResourceReader class with ReadManifestText method for reading the resources files

---
+++ Perfect Maze/TResourceReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perfect_maze
+{
+    public static class TResourceReader
+    {
+        public static string ReadManifestText(string dotPath)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            string rootNamespace = assembly.GetName().Name;
+            string resourceName = $"{rootNamespace}.{dotPath}";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                    throw new FileNotFoundException($"File not found: {resourceName}");
+                using (StreamReader reader = new StreamReader(stream))
+                    return reader.ReadToEnd();
+            }
+        }
+    }
+}
