98afe30e7ca3a53f1894d8c1d97c881fadf2d9dd
Meggi10 <magdakenobi10@gmail.com>
2026-08-22 21:00:20 +0200

    Add DiffLvls enumeration - preparation for changes in level difficulty selection

--- Perfect Maze/TSession.cs
+++ Perfect Maze/TSession.cs
@@ -9,12 +9,15 @@ namespace Perfect_maze
     public static class TSession
     {
         public static string PlayerName { get; set; }
-        public static Modes Mode { get; set; } 
+        public static Modes Mode { get; set; }
+        public static DiffLvls DifficultyLvl { get; set; }
         public enum Modes { Speedrun, FogOfWar}
+        public enum DiffLvls { Easy, Normal, Hard}
         public static void Clear()
         {
             PlayerName = null;
             Mode = default;
+            DifficultyLvl = default;
         }
     }
 }
