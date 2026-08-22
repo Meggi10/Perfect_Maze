652008b6ea5b2795ad82d814f6f6c62c69f02657
Meggi10 <magdakenobi10@gmail.com>
2026-08-22 20:51:53 +0200

    Remove Names list and all its references in code - unused code

--- Perfect Maze/TSession.cs
+++ Perfect Maze/TSession.cs
@@ -9,14 +9,12 @@ namespace Perfect_maze
     public static class TSession
     {
         public static string PlayerName { get; set; }
-        public static List<string> Names { get; set; } = new List<string>();
         public static Modes Mode { get; set; } 
         public enum Modes { Speedrun, FogOfWar}
         public static void Clear()
         {
             PlayerName = null;
             Mode = default;
-            Names.Clear();
         }
     }
 }
