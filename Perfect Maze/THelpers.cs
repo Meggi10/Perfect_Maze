788591e0a4c239c11b258c019a88b80e3709edd7
Meggi10 <magdakenobi10@gmail.com>
2026-08-22 12:14:32 +0200

    Add THelpers class to separate helping methods from the UI and other logic methods in different classes

---
+++ Perfect Maze/THelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_maze
+{
+    public static class THelpers
+    {
+        public static string GetDisplayName(TSession.Modes mode)
+        {
+            switch (mode)
+            {
+                case TSession.Modes.Speedrun:
+                    return "Speedrun";
+                case TSession.Modes.FogOfWar:
+                    return "Fog Of War";
+                default:
+                    return mode.ToString();
+            }
+        }
+    }
+}
